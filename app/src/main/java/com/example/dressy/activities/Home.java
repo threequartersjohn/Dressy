36ec78fa39d53852bd2d94e384b98f8dd02b4a54
threequartersjohn <threequartersjohn@gmail.com>
2019-05-12 16:20:01 +0100

    icon changing
    icons now change to filled when selected and back to outlines when deselected

--- app/src/main/java/com/example/dressy/activities/Home.java
+++ app/src/main/java/com/example/dressy/activities/Home.java
@@ -1,10 +1,13 @@
 package com.example.dressy.activities;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -29,12 +32,15 @@ public class Home extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navCloset:
                     selectedFragment = new homeFragment();
+                    switchIcons(R.id.navCloset);
                     break;
                 case R.id.navHome:
                     selectedFragment = new closetFragment();
+                    switchIcons(R.id.navHome);
                     break;
                 case R.id.navFavorites:
                     selectedFragment = new favoritesFragment();
+                    switchIcons(R.id.navFavorites);
                     break;
             }
 
@@ -56,5 +62,32 @@ public class Home extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
+    private void switchIcons(Integer ID){
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        Menu menu = navigation.getMenu();
+        MenuItem home = menu.findItem(R.id.navHome);
+        MenuItem favorites = menu.findItem(R.id.navFavorites);
+        MenuItem closet = menu.findItem(R.id.navCloset);
+
+        switch (ID) {
+            case R.id.navHome:
+                home.setIcon(R.drawable.baseline_home_black_18dp);
+                favorites.setIcon(R.drawable.outline_favorite_border_black_18dp);
+                closet.setIcon(R.drawable.outline_photo_library_black_18dp);
+                break;
+            case R.id.navFavorites:
+                home.setIcon(R.drawable.outline_home_black_18dp);
+                favorites.setIcon(R.drawable.baseline_favorite_black_18dp);
+                closet.setIcon(R.drawable.outline_photo_library_black_18dp);
+                break;
+                case R.id.navCloset:
+                home.setIcon(R.drawable.outline_home_black_18dp);
+                favorites.setIcon(R.drawable.outline_favorite_border_black_18dp);
+                closet.setIcon(R.drawable.baseline_photo_library_black_18dp);
+                break;
+
+            }
+        }
 
 }
